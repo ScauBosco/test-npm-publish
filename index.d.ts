8bac62d3a0fc74a931fddb1a2b5a4b1fe9363441
Huang Huang1 JiaCheng <jiachenghua1@lenovo.com>
2023-08-10 12:25:31 +0800

    feat:npm init

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+import { message } from "antd";
+
+export const codeMessage: Record<number, string>;
+
+export function showTips(code: string): void;
+
+export function isOk(code: string | number): boolean;
